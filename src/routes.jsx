0dd3668f997b876622a8cac11d3d6e97e6048b56
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-09-28 12:16:50 +0900

    update initial amount

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,6 @@ import Register from "./pages/Register";
 const ScrollToTop = React.lazy(() => import("./ScrollToTop"));
 const Home = React.lazy(() => import("./pages/Home"));
 const Login = React.lazy(() => import("./pages/Login"));
-const Deposit = React.lazy(() => import("./pages/Deposit"));
 const Signup = React.lazy(() => import("./pages/Register"));
 const NotFound = React.lazy(() => import("./pages/NotFound"));
 
@@ -18,7 +17,6 @@ function AppRoutes() {
             <Routes>
               <Route path="/" element={<Home />}></Route>              
               <Route path="/home" element={<Home />}></Route>
-              <Route path="/deposit" element={<Deposit />}></Route>              
               <Route path="/login" element={<Login />}></Route>      
               <Route path="/register" element={<Signup />}></Route>
               <Route path="*" element={<NotFound />}></Route>
